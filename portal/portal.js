19ed6fa33fc4d3980791f02bb72fb0b5ff40d24f
Thayna Corbicelis <tcorbicelis@gmail.com>
2026-01-11 08:56:33 -0300

    mudança no portal

--- portal/portal.js
+++ portal/portal.js
@@ -13,3 +13,16 @@
       star.style.animationDuration = `${Math.random() * 3 + 2}s`;
       starsContainer.appendChild(star);
     }
+
+const galaxy = document.querySelector('.galaxy-3d');
+
+document.addEventListener('mousemove', (e) => {
+  const x = (e.clientX / window.innerWidth - 0.5) * 30;
+  const y = (e.clientY / window.innerHeight - 0.5) * -30;
+
+  galaxy.style.transform = `
+    rotateY(${x}deg)
+    rotateX(${y}deg)
+  `;
+});
+
